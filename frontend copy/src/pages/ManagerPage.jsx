d9db6f8cb2e94fed50d2adcac2b444ded590b296
JamesTran <khaidinhtran123@gmail.com>
2025-03-21 18:51:08 +0700

    Feat: FE-create

---
+++ frontend copy/src/pages/ManagerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentPage = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Trang sinh viên (Student)</h1>
+            <p>Chỉ sinh viên có quyền xem trang này.</p>
+        </div>
+    );
+};
+
+export default StudentPage;
